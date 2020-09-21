ea5903ed6f18d7a095ffaad78e24d6040a42550f
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-09-21 22:32:36 +0200

    PALLADIO-476 Deleted generated code and renamed src-man to src.

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/resourceenvironment/provider/ResourceContainerItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/resourceenvironment/provider/ResourceContainerItemProvider.java
@@ -1,181 +1,29 @@
-/**
- * Copyright 2005-2009 by SDQ, IPD, University of Karlsruhe, Germany
- */
 package org.palladiosimulator.pcm.resourceenvironment.provider;
 
-import java.util.Collection;
-import java.util.List;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
-import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
-import org.eclipse.emf.edit.provider.ViewerNotification;
-import org.palladiosimulator.pcm.core.entity.provider.EntityItemProvider;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
-import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
-import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentPackage;
 
-/**
- * This is the item provider adapter for a
- * {@link org.palladiosimulator.pcm.resourceenvironment.ResourceContainer} object. <!--
- * begin-user-doc --> <!-- end-user-doc -->
- *
- * @generated
- */
-public class ResourceContainerItemProvider extends EntityItemProvider {
+public class ResourceContainerItemProvider extends ResourceContainerItemProviderGen {
 
-    /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    public static final String copyright = "Copyright 2005-2017 by palladiosimulator.org";
-
-    /**
-     * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @generated
-     */
-    public ResourceContainerItemProvider(final AdapterFactory adapterFactory) {
-        super(adapterFactory);
-    }
-
-    /**
-     * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    public List<IItemPropertyDescriptor> getPropertyDescriptors(final Object object) {
-        if (this.itemPropertyDescriptors == null) {
-            super.getPropertyDescriptors(object);
-
-            this.addHddResourceSpecificationsPropertyDescriptor(object);
-        }
-        return this.itemPropertyDescriptors;
-    }
-
-    /**
-     * This adds a property descriptor for the Hdd Resource Specifications feature. <!--
-     * begin-user-doc --> <!-- end-user-doc -->
-     *
-     * @generated
-     */
-    protected void addHddResourceSpecificationsPropertyDescriptor(final Object object) {
-        this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(
-                ((ComposeableAdapterFactory) this.adapterFactory).getRootAdapterFactory(), this.getResourceLocator(),
-                this.getString("_UI_ResourceContainer_hddResourceSpecifications_feature"),
-                this.getString("_UI_PropertyDescriptor_description",
-                        "_UI_ResourceContainer_hddResourceSpecifications_feature", "_UI_ResourceContainer_type"),
-                ResourceenvironmentPackage.Literals.RESOURCE_CONTAINER__HDD_RESOURCE_SPECIFICATIONS, true, false, true,
-                null, null, null));
-    }
-
-    /**
-     * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate
-     * feature for an {@link org.eclipse.emf.edit.command.AddCommand},
-     * {@link org.eclipse.emf.edit.command.RemoveCommand} or
-     * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}. <!--
-     * begin-user-doc --> <!-- end-user-doc -->
-     *
-     * @generated
-     */
-    @Override
-    public Collection<? extends EStructuralFeature> getChildrenFeatures(final Object object) {
-        if (this.childrenFeatures == null) {
-            super.getChildrenFeatures(object);
-            this.childrenFeatures.add(
-                    ResourceenvironmentPackage.Literals.RESOURCE_CONTAINER__ACTIVE_RESOURCE_SPECIFICATIONS_RESOURCE_CONTAINER);
-            this.childrenFeatures.add(
-                    ResourceenvironmentPackage.Literals.RESOURCE_CONTAINER__NESTED_RESOURCE_CONTAINERS_RESOURCE_CONTAINER);
-        }
-        return this.childrenFeatures;
-    }
-
-    /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    protected EStructuralFeature getChildFeature(final Object object, final Object child) {
-        // Check the type of the specified child object and return the proper feature to use for
-        // adding (see {@link AddCommand}) it as a child.
-
-        return super.getChildFeature(object, child);
-    }
-
-    /**
-     * This returns ResourceContainer.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    public Object getImage(final Object object) {
-        return this.overlayImage(object, this.getResourceLocator().getImage("full/obj16/ResourceContainer"));
-    }
-
-    /**
-     * This returns the label text for the adapted class. <!-- begin-user-doc --> FB: Adapted method
-     * to produce a more informative label for display in non-PCM editors. In PCM editors, display
-     * is not controlled by this method but through a more general mechanism (I'n not sure where
-     * it's actually controlled, maybe the
-     * org.palladiosimulator.editors.commons.tabs.PropertyLabelProvider.getText() method?) <!--
-     * end-user-doc -->
-     *
-     * @generated not
-     */
-    @Override
-    public String getText(final Object object) {
-        return ((ResourceContainer) object).getEntityName() + " [ID: " + ((ResourceContainer) object).getId() + "]"
-                + " <" + this.getString("_UI_ResourceContainer_type") + ">";
-    }
-
-    /**
-     * This handles model notifications by calling {@link #updateChildren} to update any cached
-     * children and by creating a viewer notification, which it passes to
-     * {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    public void notifyChanged(final Notification notification) {
-        this.updateChildren(notification);
-
-        switch (notification.getFeatureID(ResourceContainer.class)) {
-        case ResourceenvironmentPackage.RESOURCE_CONTAINER__ACTIVE_RESOURCE_SPECIFICATIONS_RESOURCE_CONTAINER:
-        case ResourceenvironmentPackage.RESOURCE_CONTAINER__NESTED_RESOURCE_CONTAINERS_RESOURCE_CONTAINER:
-            this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-            return;
-        }
-        super.notifyChanged(notification);
-    }
-
-    /**
-     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that
-     * can be created under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    @Override
-    protected void collectNewChildDescriptors(final Collection<Object> newChildDescriptors, final Object object) {
-        super.collectNewChildDescriptors(newChildDescriptors, object);
-
-        newChildDescriptors.add(this.createChildParameter(
-                ResourceenvironmentPackage.Literals.RESOURCE_CONTAINER__ACTIVE_RESOURCE_SPECIFICATIONS_RESOURCE_CONTAINER,
-                ResourceenvironmentFactory.eINSTANCE.createProcessingResourceSpecification()));
-
-        newChildDescriptors.add(this.createChildParameter(
-                ResourceenvironmentPackage.Literals.RESOURCE_CONTAINER__ACTIVE_RESOURCE_SPECIFICATIONS_RESOURCE_CONTAINER,
-                ResourceenvironmentFactory.eINSTANCE.createHDDProcessingResourceSpecification()));
-
-        newChildDescriptors.add(this.createChildParameter(
-                ResourceenvironmentPackage.Literals.RESOURCE_CONTAINER__NESTED_RESOURCE_CONTAINERS_RESOURCE_CONTAINER,
-                ResourceenvironmentFactory.eINSTANCE.createResourceContainer()));
-    }
-
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ResourceContainerItemProvider(AdapterFactory adapterFactory)
+	{
+		super(adapterFactory);
+	}
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 */
+	@Override
+	public String getText(Object object)
+	{
+	    return (((((((ResourceContainer) (object)).getEntityName() + " [ID: ") + ((ResourceContainer) (object)).getId()) + "]") + " <") + getString("_UI_ResourceContainer_type")) + ">";
+	}
 }
