84531cd1ab7f8fb7d6db52309d4b69edbd58c99e
Nathan <nathan@hagel.dev>
2021-08-19 20:01:02 +0200

    Add newly created class

---
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/ExternalCallActionItemProvider.java
@@ -0,0 +1,17 @@
+package org.palladiosimulator.pcm.seff.provider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+
+public class ExternalCallActionItemProvider extends ExternalCallActionItemProviderGen {
+
+	public ExternalCallActionItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+	
+	@Override
+	protected void addSuccessor_AbstractActionPropertyDescriptor(Object object) {
+		// TODO Auto-generated method stub
+		super.addSuccessor_AbstractActionPropertyDescriptor(object);
+	}
+
+}
