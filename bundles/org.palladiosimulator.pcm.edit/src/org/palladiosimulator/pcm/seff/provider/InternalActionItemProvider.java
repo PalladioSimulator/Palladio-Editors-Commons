01ecd8084b3413261866101bb0d1b84e4b54c85e
Nathan <nathan@hagel.dev>
2021-08-23 06:32:24 +0200

    Implement filter methods for Successor and Predecessor of InternalActionPorvider

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/InternalActionItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/InternalActionItemProvider.java
@@ -1,7 +1,19 @@
 package org.palladiosimulator.pcm.seff.provider;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.EList;
+import org.palladiosimulator.pcm.seff.AbstractAction;
 import org.palladiosimulator.pcm.seff.InternalAction;
+import org.palladiosimulator.pcm.seff.ResourceDemandingBehaviour;
+import org.palladiosimulator.pcm.seff.impl.StartActionImpl;
+import org.palladiosimulator.pcm.seff.impl.StopActionImpl;
+
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ItemPropertyDescriptorUtils;
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ValueChoiceCalculatorBase;
 
 /**
  * Customized version of {@link InternalActionItemProviderGen}.
@@ -23,4 +35,39 @@ public class InternalActionItemProvider extends InternalActionItemProviderGen {
         return (((((((InternalAction) (object)).getEntityName() + " [ID: ") + ((InternalAction) (object)).getId())
                 + "]") + " <") + getString("_UI_InternalAction_type")) + ">";
     }
+
+	@Override
+	protected void addPredecessor_AbstractActionPropertyDescriptor(Object object) {
+		super.addPredecessor_AbstractActionPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(InternalAction.class, AbstractAction.class) {
+            @Override
+            protected Collection<?> getValueChoiceTyped(InternalAction object,
+                    List<AbstractAction> typedList) {
+            	ResourceDemandingBehaviour resourceDemandingBehaviour = object.getResourceDemandingBehaviour_AbstractAction();
+            	EList<AbstractAction> actionList = resourceDemandingBehaviour.getSteps_Behaviour();
+            	return actionList.stream()
+            			.filter(action -> !(action instanceof StopActionImpl))
+            			.collect(Collectors.toList());
+            }
+		});
+	}
+
+	@Override
+	protected void addSuccessor_AbstractActionPropertyDescriptor(Object object) {
+		super.addSuccessor_AbstractActionPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(InternalAction.class, AbstractAction.class) {
+            @Override
+            protected Collection<?> getValueChoiceTyped(InternalAction object,
+                    List<AbstractAction> typedList) {
+            	ResourceDemandingBehaviour resourceDemandingBehaviour = object.getResourceDemandingBehaviour_AbstractAction();
+            	EList<AbstractAction> actionList = resourceDemandingBehaviour.getSteps_Behaviour();
+            	return actionList.stream()
+            			.filter(action -> !(action instanceof StartActionImpl))
+            			.collect(Collectors.toList());
+            }
+		});
+	}
+    
 }
