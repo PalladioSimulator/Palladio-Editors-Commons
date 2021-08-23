5baaeae571e4017d794b1f38c17bb3c8fa85ce1c
Nathan <nathan@hagel.dev>
2021-08-23 02:40:25 +0200

    Implement a first working version, filtering only AbstractActions from the same ResourceDemandingBehavior for Successor of ExternalCallActions.

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/ExternalCallActionItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/ExternalCallActionItemProvider.java
@@ -1,6 +1,17 @@
 package org.palladiosimulator.pcm.seff.provider;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.EList;
+import org.palladiosimulator.pcm.seff.AbstractAction;
+import org.palladiosimulator.pcm.seff.ExternalCallAction;
+import org.palladiosimulator.pcm.seff.ResourceDemandingBehaviour;
+
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ItemPropertyDescriptorUtils;
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ValueChoiceCalculatorBase;
 
 public class ExternalCallActionItemProvider extends ExternalCallActionItemProviderGen {
 
@@ -10,8 +21,21 @@ public class ExternalCallActionItemProvider extends ExternalCallActionItemProvid
 	
 	@Override
 	protected void addSuccessor_AbstractActionPropertyDescriptor(Object object) {
-		// TODO Auto-generated method stub
 		super.addSuccessor_AbstractActionPropertyDescriptor(object);
-	}
+		// 1. var decoraor = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		// decorator.setValueChoiceCalculator(Anonyme Klasse new ValueChoiceDecorator(iwelche Sachen))
+		// Implementiere getValueChoiceTyped() Methode, die die gefilterte Liste zurückgibt.
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
 
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(ExternalCallAction.class, AbstractAction.class) {
+            @Override
+            protected Collection<?> getValueChoiceTyped(ExternalCallAction object,
+                    List<AbstractAction> typedList) {
+            	ResourceDemandingBehaviour rdb = object.getResourceDemandingBehaviour_AbstractAction();
+            	EList<AbstractAction> list = rdb.getSteps_Behaviour();
+            	return list;
+            }
+		});
+	}
+	
 }
