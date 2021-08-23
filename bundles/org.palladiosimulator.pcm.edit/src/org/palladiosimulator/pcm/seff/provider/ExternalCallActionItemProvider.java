add8f35ac6d9192797f93fea74b2809673f13153
Nathan <nathan@hagel.dev>
2021-08-23 03:34:38 +0200

    Implement addPredecessor_AbstractActionPropertyDescriptor method to filter only for valid Actions.

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/ExternalCallActionItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/ExternalCallActionItemProvider.java
@@ -12,6 +12,7 @@ import org.palladiosimulator.pcm.seff.AbstractAction;
 import org.palladiosimulator.pcm.seff.ExternalCallAction;
 import org.palladiosimulator.pcm.seff.ResourceDemandingBehaviour;
 import org.palladiosimulator.pcm.seff.impl.StartActionImpl;
+import org.palladiosimulator.pcm.seff.impl.StopActionImpl;
 
 import tools.mdsd.library.emfeditutils.itempropertydescriptor.ItemPropertyDescriptorUtils;
 import tools.mdsd.library.emfeditutils.itempropertydescriptor.ValueChoiceCalculatorBase;
@@ -34,6 +35,23 @@ public class ExternalCallActionItemProvider extends ExternalCallActionItemProvid
             	EList<AbstractAction> actionList = resourceDemandingBehaviour.getSteps_Behaviour();
             	return actionList.stream()
             			.filter(action -> !(action instanceof StartActionImpl))
+            			.collect(Collectors.toList());
+            }
+		});
+	}
+
+	@Override
+	protected void addPredecessor_AbstractActionPropertyDescriptor(Object object) {
+		super.addPredecessor_AbstractActionPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(ExternalCallAction.class, AbstractAction.class) {
+            @Override
+            protected Collection<?> getValueChoiceTyped(ExternalCallAction object,
+                    List<AbstractAction> typedList) {
+            	ResourceDemandingBehaviour resourceDemandingBehaviour = object.getResourceDemandingBehaviour_AbstractAction();
+            	EList<AbstractAction> actionList = resourceDemandingBehaviour.getSteps_Behaviour();
+            	return actionList.stream()
+            			.filter(action -> !(action instanceof StopActionImpl))
             			.collect(Collectors.toList());
             }
 		});
