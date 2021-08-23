d5d8f3a1f2ebef570292b331383264e91fe2820e
Nathan <nathan@hagel.dev>
2021-08-23 06:37:16 +0200

    Create StartActionItemProvider.class in src package.

---
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/StartActionItemProvider.java
@@ -0,0 +1,11 @@
+package org.palladiosimulator.pcm.seff.provider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+
+public class StartActionItemProvider extends StartActionItemProviderGen{
+
+	public StartActionItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+}
