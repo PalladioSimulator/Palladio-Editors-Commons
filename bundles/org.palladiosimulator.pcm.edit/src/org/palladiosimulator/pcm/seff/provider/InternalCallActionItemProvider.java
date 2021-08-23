7459a0f78cb393bd3313613d6d228631f308547e
Nathan <nathan@hagel.dev>
2021-08-23 06:18:07 +0200

    Create InternalCallActionProvider class in src package.

---
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/seff/provider/InternalCallActionItemProvider.java
@@ -0,0 +1,21 @@
+package org.palladiosimulator.pcm.seff.provider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+
+public class InternalCallActionItemProvider extends InternalCallActionItemProviderGen {
+
+	public InternalCallActionItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	@Override
+	protected void addPredecessor_AbstractActionPropertyDescriptor(Object object) {
+		super.addPredecessor_AbstractActionPropertyDescriptor(object);
+	}
+
+	@Override
+	protected void addSuccessor_AbstractActionPropertyDescriptor(Object object) {
+		super.addSuccessor_AbstractActionPropertyDescriptor(object);
+	}
+	
+}
