14907314741a5ec3de8b8792f019d7668a870579
Nathan <nathan@hagel.dev>
2021-08-30 01:56:35 +0200

    Implement first not yet complete versions of filtering for AssemblyConnector. Filter by only Contexts in same Environment works but Role Filtering is complicated.

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/AssemblyConnectorItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/AssemblyConnectorItemProvider.java
@@ -2,7 +2,6 @@ package org.palladiosimulator.pcm.core.composition.provider;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
@@ -10,10 +9,13 @@ import org.eclipse.emf.common.util.EList;
 import org.palladiosimulator.pcm.core.composition.AssemblyConnector;
 import org.palladiosimulator.pcm.core.composition.AssemblyContext;
 import org.palladiosimulator.pcm.core.composition.ComposedStructure;
-import org.palladiosimulator.pcm.parameter.VariableUsage;
-import org.palladiosimulator.pcm.seff.AbstractAction;
-import org.palladiosimulator.pcm.seff.ResourceDemandingBehaviour;
-import org.palladiosimulator.pcm.seff.StartAction;
+import org.palladiosimulator.pcm.core.composition.Connector;
+import org.palladiosimulator.pcm.repository.OperationInterface;
+import org.palladiosimulator.pcm.repository.OperationProvidedRole;
+import org.palladiosimulator.pcm.repository.OperationRequiredRole;
+import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import org.palladiosimulator.pcm.repository.RequiredRole;
+import org.palladiosimulator.pcm.repository.Role;
 
 import tools.mdsd.library.emfeditutils.itempropertydescriptor.ItemPropertyDescriptorUtils;
 import tools.mdsd.library.emfeditutils.itempropertydescriptor.ValueChoiceCalculatorBase;
@@ -32,6 +34,7 @@ public class AssemblyConnectorItemProvider extends AssemblyConnectorItemProvider
 			@Override
             protected Collection<?> getValueChoiceTyped(AssemblyConnector object,
                     List<AssemblyContext> typedList) {
+				Role myRole = object.getProvidedRole_AssemblyConnector();
             	ComposedStructure composedStructure = object.getParentStructure__Connector();
             	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
             	return contexts;
@@ -42,16 +45,40 @@ public class AssemblyConnectorItemProvider extends AssemblyConnectorItemProvider
 	@Override
 	protected void addProvidingAssemblyContext_AssemblyConnectorPropertyDescriptor(Object object) {
 		super.addProvidingAssemblyContext_AssemblyConnectorPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(AssemblyConnector.class, AssemblyContext.class) {
+			@Override
+            protected Collection<?> getValueChoiceTyped(AssemblyConnector object,
+                    List<AssemblyContext> typedList) {
+				Role myRole = object.getRequiredRole_AssemblyConnector();
+            	ComposedStructure composedStructure = object.getParentStructure__Connector();
+            	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
+            	return contexts;
+            }
+		});
 	}
 
 	@Override
 	protected void addProvidedRole_AssemblyConnectorPropertyDescriptor(Object object) {
 		super.addProvidedRole_AssemblyConnectorPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(AssemblyConnector.class, OperationProvidedRole.class) {
+			@Override
+            protected Collection<?> getValueChoiceTyped(AssemblyConnector object,
+                    List<OperationProvidedRole> typedList) {
+				OperationInterface myRole = object.getRequiredRole_AssemblyConnector().getRequiredInterface__OperationRequiredRole();
+            	ComposedStructure composedStructure = object.getParentStructure__Connector();
+            	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
+            	List<RepositoryComponent> basicComponents = contexts.stream().map(AssemblyContext::getEncapsulatedComponent__AssemblyContext).collect(Collectors.toList());
+            	//Wie bekomme ich hier jetzt die OperationInterfaces der basicComponents?
+            	return contexts;
+            }
+		});
 	}
 
 	@Override
 	protected void addRequiredRole_AssemblyConnectorPropertyDescriptor(Object object) {
 		super.addRequiredRole_AssemblyConnectorPropertyDescriptor(object);
 	}
-	
+
 }
