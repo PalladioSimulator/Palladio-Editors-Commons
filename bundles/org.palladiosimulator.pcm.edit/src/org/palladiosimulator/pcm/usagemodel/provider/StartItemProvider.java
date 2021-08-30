25bbfa3085bd4c910f2a7f40e6d90a5844bb072a
Nathan <nathan@hagel.dev>
2021-08-30 02:11:25 +0200

    Implement missing filtering for Start and Stop of UsageModel.

---
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/usagemodel/provider/StartItemProvider.java
@@ -0,0 +1,35 @@
+package org.palladiosimulator.pcm.usagemodel.provider;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.palladiosimulator.pcm.seff.AbstractAction;
+import org.palladiosimulator.pcm.usagemodel.AbstractUserAction;
+import org.palladiosimulator.pcm.usagemodel.Start;
+
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ItemPropertyDescriptorUtils;
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ValueChoiceCalculatorBase;
+
+public class StartItemProvider extends StartItemProviderGen{
+
+	public StartItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	@Override
+	protected void addPredecessorPropertyDescriptor(Object object) {
+		super.addPredecessorPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(Start.class, AbstractUserAction.class) {
+            @Override
+            protected Collection<?> getValueChoiceTyped(Start object,
+                    List<AbstractUserAction> typedList) {
+            	return new ArrayList<AbstractUserAction>();
+            }
+		});
+	}
+
+	
+}
---
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/usagemodel/provider/StopItemProvider.java
@@ -0,0 +1,35 @@
+package org.palladiosimulator.pcm.usagemodel.provider;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.palladiosimulator.pcm.usagemodel.AbstractUserAction;
+import org.palladiosimulator.pcm.usagemodel.Stop;
+
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ItemPropertyDescriptorUtils;
+import tools.mdsd.library.emfeditutils.itempropertydescriptor.ValueChoiceCalculatorBase;
+
+public class StopItemProvider extends StopItemProviderGen{
+
+	public StopItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	@Override
+	protected void addSuccessorPropertyDescriptor(Object object) {
+		super.addSuccessorPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(Stop.class, AbstractUserAction.class) {
+            @Override
+            protected Collection<?> getValueChoiceTyped(Stop object,
+                    List<AbstractUserAction> typedList) {
+            	return new ArrayList<AbstractUserAction>();
+            }
+		});
+	}
+	
+	
+
+}
