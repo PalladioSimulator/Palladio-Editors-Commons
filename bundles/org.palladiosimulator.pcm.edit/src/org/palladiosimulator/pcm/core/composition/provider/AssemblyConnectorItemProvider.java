bee9d3c79350bd997bbc3320e2f7fa12233811f4
Nathan <nathan@hagel.dev>
2021-09-19 15:27:18 +0200

    Implement Filter for AssemblyConnector. Yet not working 100% correctly, but basic rules implemented.

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/AssemblyConnectorItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/AssemblyConnectorItemProvider.java
@@ -3,6 +3,7 @@ package org.palladiosimulator.pcm.core.composition.provider;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
@@ -14,6 +15,7 @@ import org.palladiosimulator.pcm.core.composition.Connector;
 import org.palladiosimulator.pcm.repository.OperationInterface;
 import org.palladiosimulator.pcm.repository.OperationProvidedRole;
 import org.palladiosimulator.pcm.repository.OperationRequiredRole;
+import org.palladiosimulator.pcm.repository.ProvidedRole;
 import org.palladiosimulator.pcm.repository.RepositoryComponent;
 import org.palladiosimulator.pcm.repository.RepositoryPackage;
 import org.palladiosimulator.pcm.repository.RequiredRole;
@@ -39,7 +41,9 @@ public class AssemblyConnectorItemProvider extends AssemblyConnectorItemProvider
 				Role myRole = object.getProvidedRole_AssemblyConnector();
             	ComposedStructure composedStructure = object.getParentStructure__Connector();
             	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
-            	return contexts;
+            	return contexts.stream()
+            			.filter(Predicate.not(object.getProvidingAssemblyContext_AssemblyConnector()::equals))
+            			.collect(Collectors.toList());
             }
 		});
 	}
@@ -55,7 +59,9 @@ public class AssemblyConnectorItemProvider extends AssemblyConnectorItemProvider
 				Role myRole = object.getRequiredRole_AssemblyConnector();
             	ComposedStructure composedStructure = object.getParentStructure__Connector();
             	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
-            	return contexts;
+            	return contexts.stream()
+            			.filter(Predicate.not(object.getRequiringAssemblyContext_AssemblyConnector()::equals))
+            			.collect(Collectors.toList());
             }
 		});
 	}
@@ -68,13 +74,19 @@ public class AssemblyConnectorItemProvider extends AssemblyConnectorItemProvider
 			@Override
             protected Collection<?> getValueChoiceTyped(AssemblyConnector object,
                     List<OperationProvidedRole> typedList) {
-				OperationInterface myRole = object.getRequiredRole_AssemblyConnector().getRequiredInterface__OperationRequiredRole();
+				OperationInterface myInterface = object.getRequiredRole_AssemblyConnector().getRequiredInterface__OperationRequiredRole();
             	ComposedStructure composedStructure = object.getParentStructure__Connector();
             	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
             	Set<RepositoryComponent> components = contexts.stream().map(AssemblyContext::getEncapsulatedComponent__AssemblyContext).collect(Collectors.toSet());
-            	//Wie bekomme ich hier jetzt die OperationInterfaces der basicComponents?
-            	RepositoryPackage.Literals.OPERATION_PROVIDED_ROLE.isInstance(object);
-            	return contexts;
+            	//Wie bekomme ich hier jetzt die OperationInterfaces der basicComponents? RepositoryPackage.Literals.OPERATION_PROVIDED_ROLE.isInstance(object)
+            	List<ProvidedRole> operationProvidedRoles = components.stream()
+            			.map(RepositoryComponent::getProvidedRoles_InterfaceProvidingEntity)
+            			.flatMap(List::stream).filter(OperationProvidedRole.class::isInstance)
+            			.map(OperationProvidedRole.class::cast)
+            			.filter(opr -> opr.getProvidedInterface__OperationProvidedRole().getId() == myInterface.getId())
+            			.collect(Collectors.toList());
+            	return operationProvidedRoles;
+            	//return operationProvidedRoles.stream().filter(opr -> RepositoryPackage.Literals.OPERATION_PROVIDED_ROLE.isInstance(object)).collect(Collectors.toList());
             }
 		});
 	}
@@ -82,6 +94,24 @@ public class AssemblyConnectorItemProvider extends AssemblyConnectorItemProvider
 	@Override
 	protected void addRequiredRole_AssemblyConnectorPropertyDescriptor(Object object) {
 		super.addRequiredRole_AssemblyConnectorPropertyDescriptor(object);
+		var decorator = ItemPropertyDescriptorUtils.decorateLastDescriptor(this.itemPropertyDescriptors);
+		decorator.setValueChoiceCalculator(new ValueChoiceCalculatorBase<>(AssemblyConnector.class, OperationRequiredRole.class) {
+			@Override
+            protected Collection<?> getValueChoiceTyped(AssemblyConnector object,
+                    List<OperationRequiredRole> typedList) {
+				OperationInterface myInterface = object.getProvidedRole_AssemblyConnector().getProvidedInterface__OperationProvidedRole();
+            	ComposedStructure composedStructure = object.getParentStructure__Connector();
+            	EList<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
+            	Set<RepositoryComponent> components = contexts.stream().map(AssemblyContext::getEncapsulatedComponent__AssemblyContext).collect(Collectors.toSet());
+            	List<RequiredRole> operationRequiredRoles = components.stream()
+            			.map(RepositoryComponent::getRequiredRoles_InterfaceRequiringEntity)
+            			.flatMap(List::stream).filter(OperationRequiredRole.class::isInstance)
+            			.map(OperationRequiredRole.class::cast)
+            			.filter(opr -> opr.getRequiredInterface__OperationRequiredRole().getId() == myInterface.getId())
+            			.collect(Collectors.toList());
+            	return operationRequiredRoles;
+            }
+		});
 	}
 
 }
