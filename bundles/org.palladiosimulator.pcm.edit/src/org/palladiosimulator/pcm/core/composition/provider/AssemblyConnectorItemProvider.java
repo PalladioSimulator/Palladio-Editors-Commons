725a2e51cdd93faad5b398284b1c50c7f95caa44
Nathan <nathan@hagel.dev>
2021-10-15 00:59:33 +0200

    Minor Changes

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/AssemblyConnectorItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/AssemblyConnectorItemProvider.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
