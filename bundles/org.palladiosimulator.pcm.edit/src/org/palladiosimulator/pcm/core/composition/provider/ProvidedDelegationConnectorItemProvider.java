d044cbf6c0170ea91f4420c8495bebc0056a6c1c
Nathan <nathan@hagel.dev>
2021-11-05 01:07:25 +0100

    Implement not complete version of ProvidedDelegationConnector filter

--- bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/ProvidedDelegationConnectorItemProvider.java
+++ bundles/org.palladiosimulator.pcm.edit/src/org/palladiosimulator/pcm/core/composition/provider/ProvidedDelegationConnectorItemProvider.java
@@ -2,6 +2,7 @@ package org.palladiosimulator.pcm.core.composition.provider;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
@@ -29,19 +30,32 @@ public class ProvidedDelegationConnectorItemProvider extends ProvidedDelegationC
 			@Override
 			protected Collection<?> getValueChoiceTyped(ProvidedDelegationConnector object,
 					List<OperationProvidedRole> typedList) {
+				ComposedStructure composedStructure = object.getParentStructure__Connector();
+				Collection<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
+				Collection<OperationProvidedRole> operationProvidedRoles = contexts.stream()
+            			.map(AssemblyContext::getEncapsulatedComponent__AssemblyContext)
+            			.map(RepositoryComponent::getProvidedRoles_InterfaceProvidingEntity)
+            			.flatMap(List::stream).map(OperationProvidedRole.class::cast)
+            			.collect(Collectors.toSet());
+				
 				AssemblyContext context = object.getAssemblyContext_ProvidedDelegationConnector();
 				if (context != null) {
-					return context.getEncapsulatedComponent__AssemblyContext().getProvidedRoles_InterfaceProvidingEntity();
+					Collection<OperationProvidedRole> contextsRoles = context.getEncapsulatedComponent__AssemblyContext()
+							.getProvidedRoles_InterfaceProvidingEntity()
+							.stream()
+							.filter(OperationProvidedRole.class::isInstance)
+							.map(OperationProvidedRole.class::cast)
+							.collect(Collectors.toList());
+					operationProvidedRoles = contextsRoles;
 				}
-				if (object.getInnerProvidedRole_ProvidedDelegationConnector() == null) {
-					return typedList;
+				OperationProvidedRole outerRole = object.getOuterProvidedRole_ProvidedDelegationConnector();
+				if (outerRole != null) {
+					operationProvidedRoles = operationProvidedRoles.stream()
+					.filter(opr -> opr.getProvidedInterface__OperationProvidedRole().isAssignableFrom(outerRole.getProvidedInterface__OperationProvidedRole()))
+					.collect(Collectors.toList());
 				}
-				Collection<AssemblyContext> contexts = object.getParentStructure__Connector().getAssemblyContexts__ComposedStructure();
-            	Collection<RepositoryComponent> components = contexts.stream().map(AssemblyContext::getEncapsulatedComponent__AssemblyContext).collect(Collectors.toList());
-            	return components.stream()
-            			.map(RepositoryComponent::getProvidedRoles_InterfaceProvidingEntity).flatMap(List::stream).collect(Collectors.toList());
+				return operationProvidedRoles;
 			}
-			
 		});
 	}
 
@@ -66,7 +80,6 @@ public class ProvidedDelegationConnectorItemProvider extends ProvidedDelegationC
             	return components.stream()
             			.map(RepositoryComponent::getProvidedRoles_InterfaceProvidingEntity).flatMap(List::stream).collect(Collectors.toList());
 			}
-			
 		});
 	}
 
@@ -79,8 +92,30 @@ public class ProvidedDelegationConnectorItemProvider extends ProvidedDelegationC
 			@Override
 			protected Collection<?> getValueChoiceTyped(ProvidedDelegationConnector object,
 					List<AssemblyContext> typedList) {
+				//OuterRoles gehen verloren
 				ComposedStructure composedStructure = object.getParentStructure__Connector();
 				List<AssemblyContext> contexts = composedStructure.getAssemblyContexts__ComposedStructure();
+				OperationProvidedRole outerRole = object.getOuterProvidedRole_ProvidedDelegationConnector();
+				if (outerRole != null) {
+					Predicate<OperationProvidedRole> predicate = role -> role
+		    				.getProvidedInterface__OperationProvidedRole()
+		    				.isAssignableFrom(outerRole.getProvidedInterface__OperationProvidedRole());
+					contexts = contexts
+            				.stream()
+            				.filter(context -> context.getEncapsulatedComponent__AssemblyContext()
+            						.getProvidedRoles_InterfaceProvidingEntity().stream().filter(OperationProvidedRole.class::isInstance)
+            						.map(OperationProvidedRole.class::cast).anyMatch(predicate))
+            				.collect(Collectors.toList());
+				}
+				OperationProvidedRole innerRole = object.getInnerProvidedRole_ProvidedDelegationConnector();
+				if (innerRole != null) {
+					contexts = contexts.stream().filter(c -> c.getEncapsulatedComponent__AssemblyContext()
+							.getProvidedRoles_InterfaceProvidingEntity()
+							.stream()
+							.filter(OperationProvidedRole.class::isInstance)
+							.map(OperationProvidedRole.class::cast)
+							.anyMatch(role -> role == innerRole)).collect(Collectors.toList());
+				}
 				return contexts;
 			}
 			
