bcc2e70fb4577f503acccce1ea6d930ca237c7bf
Nathan <nathan@hagel.dev>
2021-12-10 04:35:02 +0100

    Implement tests for DelegationConnectors and update system testmodel.

--- tests/org.palladiosimulator.pcm.edit.tests/src/org/palladiosimulator/pcm/edit/provider/ProvidedDelegationConnectorItemProviderTest.java
+++ tests/org.palladiosimulator.pcm.edit.tests/src/org/palladiosimulator/pcm/edit/provider/ProvidedDelegationConnectorItemProviderTest.java
@@ -5,48 +5,46 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.palladiosimulator.pcm.core.composition.AssemblyContext;
 import org.palladiosimulator.pcm.core.composition.CompositionPackage;
 import org.palladiosimulator.pcm.core.composition.DelegationConnector;
 import org.palladiosimulator.pcm.core.composition.ProvidedDelegationConnector;
 import org.palladiosimulator.pcm.core.composition.provider.CompositionItemProviderAdapterFactory;
 import org.palladiosimulator.pcm.core.composition.provider.ProvidedDelegationConnectorItemProvider;
 import org.palladiosimulator.pcm.repository.OperationProvidedRole;
+import org.palladiosimulator.pcm.repository.ProvidedRole;
+import org.palladiosimulator.pcm.repository.Repository;
 import org.palladiosimulator.pcm.repository.RepositoryFactory;
 import org.palladiosimulator.pcm.system.System;
 
+import de.uka.ipd.sdq.identifier.Identifier;
 import tools.mdsd.junit5utils.extensions.PlatformStandaloneExtension;
 
 @ExtendWith(PlatformStandaloneExtension.class)
 
 public class ProvidedDelegationConnectorItemProviderTest {
 
-    // Problem bei diesem Test, gleich wie bei roles von assemblyconnectortest: Repository wird
-    // nicht geladen, deshalb hat der Connector vermeintlich keine Roles.
-    @Disabled
     @Test
     public void testProvidedDelegationConnectorItemProvideraddInnerProvidedRoleProvidedDelegationConnectorPropertyDescriptor() {
         System testSystem = TestItemProviderUtilities.loadSystem();
-        RepositoryFactory repositoryFactory = RepositoryFactory.eINSTANCE;
-        // Repository testRepository = loadRepository();
+        Repository testRepository = TestItemProviderUtilities.loadRepository();
         DelegationConnector connector1 = TestItemProviderUtilities.getDelegationConnector("_wVTYoJelEeajsMFpiDxCKw",
                 testSystem);
         ProvidedDelegationConnector testConnector = (ProvidedDelegationConnector) connector1;
         assertNotNull(testConnector);
 
         // Create expected result list
-        Collection<OperationProvidedRole> expected = new ArrayList<OperationProvidedRole>();
-        // Loading Repository somehow is not working => Create OPR with same ID and compare on ID.
-        OperationProvidedRole providingRole = repositoryFactory.createOperationProvidedRole();
-        providingRole.setId("_frEeAHD2EeSA4fySuX9I2Q");
-        expected.add(providingRole);
+        Collection<ProvidedRole> expected = new ArrayList<ProvidedRole>();
+        ProvidedRole providedRole =
+        		TestItemProviderUtilities.getProvidedRole("_frEeAHD2EeSA4fySuX9I2Q", testRepository);
         expected.add(null);
-        testConnector.setInnerProvidedRole_ProvidedDelegationConnector(providingRole);
-
+        expected.add(providedRole);
         // Get result
         CompositionItemProviderAdapterFactory adapterFactory = new CompositionItemProviderAdapterFactory();
         ProvidedDelegationConnectorItemProvider provider = new ProvidedDelegationConnectorItemProvider(adapterFactory);
@@ -55,12 +53,65 @@ public class ProvidedDelegationConnectorItemProviderTest {
                 CompositionPackage.Literals.PROVIDED_DELEGATION_CONNECTOR__INNER_PROVIDED_ROLE_PROVIDED_DELEGATION_CONNECTOR);
 
         assertNotNull(descriptor);
-        var actual = descriptor.getChoiceOfValues(testConnector);
-        assertNotNull(actual.size());
-        java.lang.System.out.println(expected.size());
-        java.lang.System.out.println(actual.size());
+        var actual = (List<Identifier>) descriptor.getChoiceOfValues(testConnector);
+        assertTrue(expected.size() == actual.size());
+        assertTrue(actual.get(1).getId().equals(providedRole.getId()));
+    }
+    
+    @Test
+    public void testProvidedDelegationConnectorItemProvideraddOuterProvidedRoleProvidedDelegationConnectorPropertyDescriptor() {
+        System testSystem = TestItemProviderUtilities.loadSystem();
+        DelegationConnector connector1 = TestItemProviderUtilities.getDelegationConnector("_wVTYoJelEeajsMFpiDxCKw",
+                testSystem);
+        ProvidedDelegationConnector testConnector = (ProvidedDelegationConnector) connector1;
+        assertNotNull(testConnector);
+
+        // Create expected result list
+        Collection<ProvidedRole> expected = new ArrayList<ProvidedRole>();
+        ProvidedRole providedRole =
+        		TestItemProviderUtilities.getOuterProvidedRole("_b9NvYHD6EeSA4fySuX9I2S", testSystem);
+        expected.add(null);
+        expected.add(providedRole);
+        // Get result
+        CompositionItemProviderAdapterFactory adapterFactory = new CompositionItemProviderAdapterFactory();
+        ProvidedDelegationConnectorItemProvider provider = new ProvidedDelegationConnectorItemProvider(adapterFactory);
+
+        IItemPropertyDescriptor descriptor = provider.getPropertyDescriptor(testConnector,
+                CompositionPackage.Literals.PROVIDED_DELEGATION_CONNECTOR__OUTER_PROVIDED_ROLE_PROVIDED_DELEGATION_CONNECTOR);
+
+        assertNotNull(descriptor);
+        var actual = (List<Identifier>) descriptor.getChoiceOfValues(testConnector);
+        assertTrue(expected.size() == actual.size());
+        assertTrue(actual.get(1).getId().equals(providedRole.getId()));
+    }
+    
+    @Test
+    public void testProvidedDelegationConnectorItemProvideraddAssemblyContextProvidedDelegationConnectorPropertyDescriptor() {
+    	System testSystem = TestItemProviderUtilities.loadSystem();
+        DelegationConnector connector1 = TestItemProviderUtilities.getDelegationConnector("_wVTYoJelEeajsMFpiDxCKw",
+                testSystem);
+        ProvidedDelegationConnector testConnector = (ProvidedDelegationConnector) connector1;
+        assertNotNull(testConnector);
+
+        // Create expected result list
+        Collection<AssemblyContext> expected = new ArrayList<AssemblyContext>();
+        AssemblyContext assemblyMediaManagement = TestItemProviderUtilities.getAssemblyContext("_bm3CcAjmEeyy5MM7BmZ05A", testSystem);
+        AssemblyContext mediaManagement = TestItemProviderUtilities.getAssemblyContext("_C1kK8HD5EeSA4fySuX9I2Z", testSystem);
+        expected.add(null);
+        expected.add(assemblyMediaManagement);
+        expected.add(mediaManagement);
+        // Get result
+        CompositionItemProviderAdapterFactory adapterFactory = new CompositionItemProviderAdapterFactory();
+        ProvidedDelegationConnectorItemProvider provider = new ProvidedDelegationConnectorItemProvider(adapterFactory);
+
+        IItemPropertyDescriptor descriptor = provider.getPropertyDescriptor(testConnector,
+                CompositionPackage.Literals.PROVIDED_DELEGATION_CONNECTOR__ASSEMBLY_CONTEXT_PROVIDED_DELEGATION_CONNECTOR);
+
+        assertNotNull(descriptor);
+        var actual = (List<Identifier>) descriptor.getChoiceOfValues(testConnector);
         assertTrue(expected.size() == actual.size());
         assertTrue(expected.containsAll(actual));
     }
+    
 
 }
--- tests/org.palladiosimulator.pcm.edit.tests/src/org/palladiosimulator/pcm/edit/provider/RequiredDelegationConnectorItemProviderTest.java
+++ tests/org.palladiosimulator.pcm.edit.tests/src/org/palladiosimulator/pcm/edit/provider/RequiredDelegationConnectorItemProviderTest.java
@@ -1,5 +1,120 @@
 package org.palladiosimulator.pcm.edit.provider;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.palladiosimulator.pcm.core.composition.AssemblyContext;
+import org.palladiosimulator.pcm.core.composition.CompositionPackage;
+import org.palladiosimulator.pcm.core.composition.DelegationConnector;
+import org.palladiosimulator.pcm.core.composition.ProvidedDelegationConnector;
+import org.palladiosimulator.pcm.core.composition.RequiredDelegationConnector;
+import org.palladiosimulator.pcm.core.composition.provider.CompositionItemProviderAdapterFactory;
+import org.palladiosimulator.pcm.core.composition.provider.ProvidedDelegationConnectorItemProvider;
+import org.palladiosimulator.pcm.core.composition.provider.RequiredDelegationConnectorItemProvider;
+import org.palladiosimulator.pcm.repository.ProvidedRole;
+import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryFactory;
+import org.palladiosimulator.pcm.repository.RequiredRole;
+import org.palladiosimulator.pcm.system.System;
+
+import de.uka.ipd.sdq.identifier.Identifier;
+import tools.mdsd.junit5utils.extensions.PlatformStandaloneExtension;
+
+@ExtendWith(PlatformStandaloneExtension.class)
+
+
 public class RequiredDelegationConnectorItemProviderTest {
 
+	@Test
+    public void testRequiredDelegationConnectorItemProvideraddInnerRequiredRoleRequiredDelegationConnectorPropertyDescriptor() {
+        System testSystem = TestItemProviderUtilities.loadSystem();
+        Repository testRepository = TestItemProviderUtilities.loadRepository();
+        RepositoryFactory repositoryFactory = RepositoryFactory.eINSTANCE;
+        // Repository testRepository = loadRepository();
+        DelegationConnector connector1 = TestItemProviderUtilities.getDelegationConnector("_6AQ2YFliEeyJ8NxP0zEPRw",
+                testSystem);
+        RequiredDelegationConnector testConnector = (RequiredDelegationConnector) connector1;
+        assertNotNull(testConnector);
+
+        // Create expected result list
+        Collection<RequiredRole> expected = new ArrayList<RequiredRole>();
+        RequiredRole requiredRole =
+        		TestItemProviderUtilities.getRequiredRole("_5VbJoGrUEeajAsHLADMRRw", testRepository);
+        expected.add(null);
+        expected.add(requiredRole);
+        // Get result
+        CompositionItemProviderAdapterFactory adapterFactory = new CompositionItemProviderAdapterFactory();
+        RequiredDelegationConnectorItemProvider provider = new RequiredDelegationConnectorItemProvider(adapterFactory);
+
+        IItemPropertyDescriptor descriptor = provider.getPropertyDescriptor(testConnector,
+                CompositionPackage.Literals.REQUIRED_DELEGATION_CONNECTOR__INNER_REQUIRED_ROLE_REQUIRED_DELEGATION_CONNECTOR);
+
+        assertNotNull(descriptor);
+        var actual = (List<Identifier>) descriptor.getChoiceOfValues(testConnector);
+        assertTrue(expected.size() == actual.size());
+        assertTrue(actual.get(1).getId().equals(requiredRole.getId()));
+    }
+	
+	@Test
+    public void testRequiredDelegationConnectorItemProvideraddOuterRequiredRoleRequiredDelegationConnectorPropertyDescriptor() {
+        System testSystem = TestItemProviderUtilities.loadSystem();
+        DelegationConnector connector1 = TestItemProviderUtilities.getDelegationConnector("_6AQ2YFliEeyJ8NxP0zEPRw",
+                testSystem);
+        RequiredDelegationConnector testConnector = (RequiredDelegationConnector) connector1;
+        assertNotNull(testConnector);
+
+        // Create expected result list
+        Collection<RequiredRole> expected = new ArrayList<RequiredRole>();
+        RequiredRole requiredRole =
+        		TestItemProviderUtilities.getOuterRequiredRole("_v2fKE1liEeyJ8NxP0zEPRw", testSystem);
+        expected.add(null);
+        expected.add(requiredRole);
+        // Get result
+        CompositionItemProviderAdapterFactory adapterFactory = new CompositionItemProviderAdapterFactory();
+        RequiredDelegationConnectorItemProvider provider = new RequiredDelegationConnectorItemProvider(adapterFactory);
+
+        IItemPropertyDescriptor descriptor = provider.getPropertyDescriptor(testConnector,
+                CompositionPackage.Literals.REQUIRED_DELEGATION_CONNECTOR__OUTER_REQUIRED_ROLE_REQUIRED_DELEGATION_CONNECTOR);
+
+        assertNotNull(descriptor);
+        var actual = (List<Identifier>) descriptor.getChoiceOfValues(testConnector);
+        assertTrue(expected.size() == actual.size());
+        assertTrue(actual.get(1).getId().equals(requiredRole.getId()));
+    }
+	
+	@Test
+    public void testRequiredDelegationConnectorItemProvideraddAssemblyContextRequiredDelegationConnectorPropertyDescriptor() {
+    	System testSystem = TestItemProviderUtilities.loadSystem();
+        DelegationConnector connector1 = TestItemProviderUtilities.getDelegationConnector("_6AQ2YFliEeyJ8NxP0zEPRw",
+                testSystem);
+        RequiredDelegationConnector testConnector = (RequiredDelegationConnector) connector1;
+        assertNotNull(testConnector);
+
+        // Create expected result list
+        Collection<AssemblyContext> expected = new ArrayList<AssemblyContext>();
+        AssemblyContext instantDownloadCacheContext = TestItemProviderUtilities.getAssemblyContext("_IulGALW1EeapTpBxO5_ZIS", testSystem);
+        expected.add(null);
+        expected.add(instantDownloadCacheContext);
+        // Get result
+        CompositionItemProviderAdapterFactory adapterFactory = new CompositionItemProviderAdapterFactory();
+        RequiredDelegationConnectorItemProvider provider = new RequiredDelegationConnectorItemProvider(adapterFactory);
+
+        IItemPropertyDescriptor descriptor = provider.getPropertyDescriptor(testConnector,
+                CompositionPackage.Literals.REQUIRED_DELEGATION_CONNECTOR__ASSEMBLY_CONTEXT_REQUIRED_DELEGATION_CONNECTOR);
+
+        assertNotNull(descriptor);
+        var actual = (List<Identifier>) descriptor.getChoiceOfValues(testConnector);
+        assertTrue(expected.size() == actual.size());
+        assertTrue(expected.containsAll(actual));
+    }
+	
+	
 }
